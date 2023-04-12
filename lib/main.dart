332a742e666b3ae5987d0ba0a24fc982725df633
Carlos-Eduardo-Guedes-01 <carloseduardoguedes89981456761@gmail.com>
2023-04-12 16:43:57 -0300

    commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(
-    DevicePreview(enabled: true, builder: (context) => const calculadora()));
+void main() =>
+    runApp(DevicePreview(enabled: true, builder: (context) => calculadora()));
 
 // ignore: camel_case_types
 class calculadora extends StatefulWidget {
@@ -14,8 +14,19 @@ class calculadora extends StatefulWidget {
 
 // ignore: camel_case_types
 class _calculadoraState extends State<calculadora> {
-  String num = '0';
-  void calcular(String tecla) {
+  //contador de opração
+  int cont = 0;
+  //variavel de label
+  String num = '';
+  //primeiro valor do calculo
+  var num2 = '';
+  //segundo valor do calculo
+  var num3 = '';
+  //operação desejada
+  var op = '';
+  //historico de calculos
+  var historico = [];
+  void calcular(String tecla, String numero, var vet) {
     switch (tecla) {
       case '0':
       case '1':
@@ -27,11 +38,119 @@ class _calculadoraState extends State<calculadora> {
       case '7':
       case '8':
       case '9':
+      case ',':
         setState(() {
+          //if (cont == 0) {
           num += tecla;
+          //} else if (cont > 0) {
+          //  num2 = tecla;
+          //   cont = 0;
+          //}
         });
         break;
+      case 'AC':
+        setState(() {
+          num = '';
+        });
+        break;
+      case '+/-':
+        setState(() {
+          double valor = 0;
+          valor = double.parse(numero);
+          valor *= -1;
+          num = valor.toString();
+        });
+        break;
+      case 'X':
+        setState(() {
+          num2 = numero;
+          op = tecla;
+          calcular('AC', numero, vet);
+          cont += 1;
+          op = 'X';
+        });
+        break;
+      case '+':
+        setState(() {
+          num2 = numero;
+          op = tecla;
+          calcular('AC', numero, vet);
+          cont += 1;
+          op = '+';
+        });
+        break;
+      case '-':
+        setState(() {
+          num2 = numero;
+          op = tecla;
+          calcular('AC', numero, vet);
+          cont += 1;
+          op = '-';
+        });
+        break;
+      case '%':
+        setState(() {
+          num2 = numero;
+          op = tecla;
+          calcular('AC', numero, vet);
+          cont += 1;
+          op = '%';
+        });
+        break;
+      case '/':
+        setState(() {
+          num2 = numero;
+          op = tecla;
+          calcular('AC', numero, vet);
+          cont += 1;
+          op = '/';
+        });
+        break;
+      case '=':
+        if (op == 'X') {
+          setState(() {
+            num3 = numero;
+            double res = 0;
+            res = double.parse(num2) * double.parse(num3);
+            num = res.toString();
+          });
+        } else if (op == '%') {
+          setState(() {
+            num3 = numero;
+            double res = 0;
+            res = double.parse(num2) * double.parse(num3) / 100;
+            num = res.toString();
+          });
+        } else if (op == '/') {
+          setState(() {
+            num3 = numero;
+            double res = 0;
+            res = double.parse(num2) / double.parse(num3);
+            num = res.toString();
+          });
+        } else if (op == '-') {
+          setState(() {
+            num3 = numero;
+            double res = 0;
+            res = double.parse(num2) - double.parse(num3);
+            num = res.toString();
+          });
+        } else if (op == '+') {
+          setState(() {
+            num3 = numero;
+            double res = 0;
+            res = double.parse(num2) + double.parse(num3);
+            num = res.toString();
+          });
+        } else {
+          setState(() {
+            num = 'Unvalidate';
+          });
+          historico.add(num2 + op + num3 + tecla + num);
+        }
+        break;
       default:
+        num = '';
         break;
     }
   }
@@ -55,32 +174,51 @@ class _calculadoraState extends State<calculadora> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text(
-                  num,
-                  style: TextStyle(fontSize: 72),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      num,
+                      style: TextStyle(fontSize: 72),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  'AC',
-                  style: TextStyle(
-                    fontSize: 48,
+                GestureDetector(
+                  onTap: () => calcular('AC', '', ''),
+                  child: const Text(
+                    'AC',
+                    style: TextStyle(
+                      fontSize: 48,
+                    ),
                   ),
                 ),
-                Text(
-                  '+/-',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('+/-', num, ''),
+                  child: Text(
+                    '+/-',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '%',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('%', num, ''),
+                  child: Text(
+                    '%',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '/',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('/', num, ''),
+                  child: Text(
+                    '/',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
@@ -89,7 +227,7 @@ class _calculadoraState extends State<calculadora> {
               children: [
                 GestureDetector(
                   onTap: () {
-                    calcular('7');
+                    calcular('7', '', '');
                   },
                   child: Text(
                     '7',
@@ -97,82 +235,123 @@ class _calculadoraState extends State<calculadora> {
                   ),
                 ),
                 GestureDetector(
-                  onTap: () => calcular('8'),
+                  onTap: () => calcular('8', '', ''),
                   child: Text(
                     '8',
                     style: TextStyle(fontSize: 48),
                   ),
                 ),
-                Text(
-                  '9',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('9', '', ''),
+                  child: Text(
+                    '9',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  'X',
-                  style: TextStyle(fontSize: 48),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(
-                  '4',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '5',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '6',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '-',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('X', num, ''),
+                  child: Text(
+                    'X',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  '1',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('4', '', ''),
+                  child: Text(
+                    '4',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '2',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('5', '', ''),
+                  child: Text(
+                    '5',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '3',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('6', '', ''),
+                  child: Text(
+                    '6',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '=',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () {
+                    calcular('-', num, '');
+                  },
+                  child: Text(
+                    '-',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text(
-                  '0',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('1', '', ''),
+                  child: Text(
+                    '1',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('2', '', ''),
+                  child: Text(
+                    '2',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('3', '', ''),
+                  child: Text(
+                    '3',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => calcular('=', num, ''),
+                  child: Text(
+                    '=',
+                    style: TextStyle(fontSize: 48),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => calcular('0', '', ''),
+                  child: Text(
+                    '0',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
                 Text(
                   '',
                   style: TextStyle(fontSize: 48),
                 ),
-                Text(
-                  ',',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('.', '', ''),
+                  child: Text(
+                    ',',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
-                Text(
-                  '+',
-                  style: TextStyle(fontSize: 48),
+                GestureDetector(
+                  onTap: () => calcular('+', num, ''),
+                  child: Text(
+                    '+',
+                    style: TextStyle(fontSize: 48),
+                  ),
                 ),
               ],
             ),
